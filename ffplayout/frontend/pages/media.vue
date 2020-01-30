c8b90c02a6ba85c2ca593d94878a0dc9d87df3cd
jb-alvarado <jb@pixelcrusher.de>
2020-01-30 21:25:10 +0100

    add media route

---
+++ ffplayout/frontend/pages/media.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    components: {},
+
+    created () {
+        this.getPath()
+    },
+
+    methods: {
+        async getPath () {
+            await this.$store.dispatch('auth/inspectToken')
+            // const response = await this.$axios.get('apt/')
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
