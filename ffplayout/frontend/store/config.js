feac9d0a8023d9c12f2bf37f9b8cc65a6d21caf0
jb-alvarado <jb@pixelcrusher.de>
2020-04-10 12:21:39 +0200

    data goes in its own object

--- ffplayout/frontend/store/config.js
+++ ffplayout/frontend/store/config.js
@@ -13,27 +13,12 @@ export const actions = {
         const response = await this.$axios.get('api/config/?config', { headers: { Authorization: 'Bearer ' + rootState.auth.jwtToken } })
 
         if (response.data) {
-            // post_ffmpeg_param is normally a object, for the form we convert it to a string
-            response.data.out.post_ffmpeg_param = JSON.stringify(response.data.out.post_ffmpeg_param).replace('{', '').replace('}', '').replace(/":/g, ' ').replace(/","/g, ' ').replace(/"/g, '')
             commit('UPDATE_CONFIG', response.data)
         }
     },
 
     async setConfig ({ commit, state, rootState }, obj) {
-        const ffmpegParam = new Map()
-        const newObj = JSON.parse(JSON.stringify(obj))
-        const params = obj.out.post_ffmpeg_param.split(' ')
-
-        for (let i = 0; i < params.length; i++) {
-            if (i % 2) {
-                continue
-            } else {
-                ffmpegParam.set(params[i], (params[i + 1]) ? params[i + 1] : null)
-            }
-        }
-
-        newObj.out.post_ffmpeg_param = Object.fromEntries(ffmpegParam)
-        const response = await this.$axios.post('api/config/', { body: { name: 'andi', des: 'welcome' } })
+        const response = await this.$axios.post('api/config/?config', { data: obj }, { headers: { Authorization: 'Bearer ' + rootState.auth.jwtToken } })
         console.log(response)
     }
 }
