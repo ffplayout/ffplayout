cc0d1ba51b41f575cb14188e0a25309c19e76362
jb-alvarado <jb@pixelcrusher.de>
2020-04-13 21:34:47 +0200

    add global filter

---
+++ ffplayout/frontend/plugins/filters.js
@@ -0,0 +1,7 @@
+import Vue from 'vue'
+
+Vue.filter('toMin', function (sec) {
+    const minutes = Math.floor(sec / 60)
+    const seconds = Math.round(sec - minutes * 60)
+    return `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')} min`
+})
