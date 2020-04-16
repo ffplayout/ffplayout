5f949ea4c697b938e03d2693cf94497617aa55bc
Jonathan Baecker <jb@pixelcrusher.de>
2020-04-16 18:30:04 +0200

    add alert, check password

--- ffplayout/frontend/pages/configure.vue
+++ ffplayout/frontend/pages/configure.vue
@@ -3,7 +3,7 @@
         <Menu />
         <b-card no-body>
             <b-tabs pills card vertical>
-                <b-tab title="GUI" active>
+                <b-tab title="GUI" active @click="resetAlert()">
                     <b-container class="config-container">
                         <b-form v-if="configGui" @submit="onSubmitGui">
                             <b-form-group
@@ -14,24 +14,44 @@
                                 class="config-group"
                             >
                                 <b-form-group
-                                    v-for="(prop, name, idx) in configGui[0]"
+                                    v-for="(prop, name, idx) in configGui"
                                     :key="idx"
                                     label-cols-sm="2"
                                     :label="name"
                                     label-align-sm="right"
                                     :label-for="name"
                                 >
-                                    <b-form-select v-if="name === 'net_interface'" :id="name" v-model="configGui[0][name]" :options="netChoices" :value="prop" />
-                                    <b-form-input v-else :id="name" v-model="configGui[0][name]" :value="prop" />
+                                    <b-form-tags
+                                        v-if="name === 'extra_extensions'"
+                                        v-model="configGui[name]"
+                                        :input-id="name"
+                                        separator=" ,;"
+                                        :placeholder="`add ${name}...`"
+                                        class="mb-2 tags-list"
+                                    />
+                                    <b-form-text v-if="name === 'extra_extensions'">
+                                        Visible extensions only for the GUI and not the playout
+                                    </b-form-text>
+                                    <b-form-select v-else-if="name === 'net_interface'" :id="name" v-model="configGui[name]" :options="netChoices" :value="prop" />
+                                    <b-form-input v-else :id="name" v-model="configGui[name]" :value="prop" />
                                 </b-form-group>
                             </b-form-group>
-                            <b-button type="submit" variant="primary">
-                                Save
-                            </b-button>
+                            <b-row>
+                                <b-col cols="1" style="min-width: 85px">
+                                    <b-button type="submit" variant="primary">
+                                        Save
+                                    </b-button>
+                                </b-col>
+                                <b-col>
+                                    <b-alert v-model="showAlert" :variant="alertVariant" dismissible>
+                                        {{ alertMsg }}
+                                    </b-alert>
+                                </b-col>
+                            </b-row>
                         </b-form>
                     </b-container>
                 </b-tab>
-                <b-tab title="Playout">
+                <b-tab title="Playout" @click="resetAlert()">
                     <b-container class="config-container">
                         <b-form v-if="configPlayout" @submit="onSubmitPlayout">
                             <b-form-group
@@ -87,25 +107,41 @@
                                     <b-form-tags
                                         v-else-if="Array.isArray(prop)"
                                         v-model="configPlayout[key][name]"
-                                        input-id="tags-basic"
+                                        :input-id="name"
                                         separator=" ,;"
                                         :placeholder="`add ${name}...`"
-                                        class="mb-2"
+                                        class="mb-2 tags-list"
+                                    />
+                                    <b-form-input
+                                        v-else-if="name.includes('pass')"
+                                        :id="name"
+                                        v-model="configPlayout[key][name]"
+                                        type="password"
+                                        :value="prop"
                                     />
                                     <b-form-input v-else :id="name" v-model="configPlayout[key][name]" :value="prop" />
                                 </b-form-group>
                             </b-form-group>
 
-                            <b-button type="submit" variant="primary">
-                                Save
-                            </b-button>
+                            <b-row>
+                                <b-col cols="1" style="min-width: 85px">
+                                    <b-button type="submit" variant="primary">
+                                        Save
+                                    </b-button>
+                                </b-col>
+                                <b-col>
+                                    <b-alert v-model="showAlert" :variant="alertVariant" dismissible>
+                                        {{ alertMsg }}
+                                    </b-alert>
+                                </b-col>
+                            </b-row>
                         </b-form>
                     </b-container>
                 </b-tab>
-                <b-tab title="User">
+                <b-tab title="User" @click="resetAlert()">
                     <b-card-text>
                         <b-container class="config-container">
-                            <b-form v-if="configUser" @submit="onSubmitGui">
+                            <b-form v-if="configUser" @submit="onSubmitUser">
                                 <b-form-group
                                     label-cols-lg="2"
                                     label="User Configuration"
@@ -114,15 +150,20 @@
                                     class="config-group"
                                 >
                                     <b-form-group
-                                        v-for="(prop, name, idx) in configUser[0]"
-                                        :key="idx"
                                         label-cols-sm="2"
-                                        :label="name"
+                                        :label="'username'"
                                         label-align-sm="right"
-                                        :label-for="name"
+                                        :label-for="'username'"
                                     >
-                                        <b-form-input v-if="name === 'username'" :id="name" v-model="configUser[0][name]" :value="prop" disabled />
-                                        <b-form-input v-else :id="name" v-model="configUser[0][name]" :value="prop" />
+                                        <b-form-input id="username" v-model="configUser['username']" :value="configUser['username']" disabled />
+                                    </b-form-group>
+                                    <b-form-group
+                                        label-cols-sm="2"
+                                        :label="'email'"
+                                        label-align-sm="right"
+                                        :label-for="'email'"
+                                    >
+                                        <b-form-input id="email" v-model="configUser['email']" :value="configUser['email']" />
                                     </b-form-group>
                                     <b-form-group
                                         label-cols-sm="2"
@@ -149,9 +190,18 @@
                                         <b-form-input id="confirmPass" v-model="confirmPass" type="password" />
                                     </b-form-group>
                                 </b-form-group>
-                                <b-button type="submit" variant="primary">
-                                    Save
-                                </b-button>
+                                <b-row>
+                                    <b-col cols="1" style="min-width: 85px">
+                                        <b-button type="submit" variant="primary">
+                                            Save
+                                        </b-button>
+                                    </b-col>
+                                    <b-col>
+                                        <b-alert v-model="showAlert" :variant="alertVariant" dismissible>
+                                            {{ alertMsg }}
+                                        </b-alert>
+                                    </b-col>
+                                </b-row>
                             </b-form>
                         </b-container>
                     </b-card-text>
@@ -190,12 +240,14 @@ export default {
             configGui: store.state.config.configGui,
             netChoices: store.state.config.netChoices,
             configPlayout: store.state.config.configPlayout,
-            currentUser: store.state.config.currentUser,
             configUser: store.state.config.configUser,
             oldPass: null,
             newPass: null,
             confirmPass: null,
-            showLogin: login
+            showLogin: login,
+            showAlert: false,
+            alertVariant: 'success',
+            alertMsg: ''
         }
     },
 
@@ -208,12 +260,61 @@ export default {
         async onSubmitGui (evt) {
             evt.preventDefault()
             await this.$store.dispatch('auth/inspectToken')
-            this.$store.dispatch('config/setGuiConfig', this.configGui[0])
+            const update = await this.$store.dispatch('config/setGuiConfig', this.configGui)
+
+            if (update.status === 200) {
+                this.alertVariant = 'success'
+                this.alertMsg = 'Update GUI config success!'
+            } else {
+                this.alertVariant = 'danger'
+                this.alertMsg = 'Update GUI config failed!'
+            }
+
+            this.showAlert = true
         },
         async onSubmitPlayout (evt) {
             evt.preventDefault()
             await this.$store.dispatch('auth/inspectToken')
-            this.$store.dispatch('config/setPlayoutConfig', this.configPlayout)
+            const update = await this.$store.dispatch('config/setPlayoutConfig', this.configPlayout)
+
+            if (update.status === 200) {
+                this.alertVariant = 'success'
+                this.alertMsg = 'Update playout config success!'
+            } else {
+                this.alertVariant = 'danger'
+                this.alertMsg = 'Update playout config failed!'
+            }
+
+            this.showAlert = true
+        },
+        async onSubmitUser (evt) {
+            evt.preventDefault()
+            if (this.oldPass && this.newPass && this.newPass === this.confirmPass) {
+                this.configUser.old_password = this.oldPass
+                this.configUser.new_password = this.newPass
+            }
+            await this.$store.dispatch('auth/inspectToken')
+            const update = await this.$store.dispatch('config/setUserConfig', this.configUser)
+
+            if (update.status === 200) {
+                this.alertVariant = 'success'
+                this.alertMsg = 'Update user profil success!'
+            } else {
+                this.alertVariant = 'danger'
+                this.alertMsg = 'Update user profil failed!'
+            }
+
+            this.showAlert = true
+
+            this.oldPass = null
+            this.newPass = null
+            this.confirmPass = null
+        },
+
+        resetAlert () {
+            this.showAlert = false
+            this.alertVariant = 'success'
+            this.alertMsg = ''
         }
     }
 }
