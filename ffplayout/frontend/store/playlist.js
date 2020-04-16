8b919695b0f917f00c4279cd75a07e2ac781fcc4
Jonathan Baecker <jb@pixelcrusher.de>
2020-04-16 18:27:09 +0200

    cleanup

--- ffplayout/frontend/store/playlist.js
+++ ffplayout/frontend/store/playlist.js
@@ -10,7 +10,6 @@ export const mutations = {
 
 export const actions = {
     async getPlaylist ({ commit, dispatch, state, rootState }, { dayStart, date }) {
-        console.log(date)
         const response = await this.$axios.get(`api/playlist/?date=${date}`, { headers: { Authorization: 'Bearer ' + rootState.auth.jwtToken } })
 
         const [h, m, s] = dayStart.split(':')
