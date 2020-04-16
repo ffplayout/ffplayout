c716a14330fe3e64c8ec25e8174634418a9a2ed0
Jonathan Baecker <jb@pixelcrusher.de>
2020-04-16 18:31:04 +0200

    return empty string

--- ffplayout/frontend/plugins/filters.js
+++ ffplayout/frontend/plugins/filters.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 
 Vue.filter('toMin', function (sec) {
-    const minutes = Math.floor(sec / 60)
-    const seconds = Math.round(sec - minutes * 60)
-    return `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')} min`
+    if (sec) {
+        const minutes = Math.floor(sec / 60)
+        const seconds = Math.round(sec - minutes * 60)
+        return `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')} min`
+    } else {
+        return ''
+    }
 })
