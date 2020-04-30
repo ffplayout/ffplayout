64f49414ecd50b8f936c59b365a2272fa68d7d03
jb-alvarado <jb@pixelcrusher.de>
2020-04-30 22:23:17 +0200

    fix env

--- ffplayout/manage.py
+++ ffplayout/manage.py
@@ -5,7 +5,8 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ffplayout.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                          'ffplayout.settings.development')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
