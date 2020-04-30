3c59df80326295ffad3cd2de494ca8b79befeda3
jb-alvarado <jb@pixelcrusher.de>
2020-04-30 22:23:08 +0200

    fix BASE_DIR for db

--- ffplayout/ffplayout/settings/common.py
+++ ffplayout/ffplayout/settings/common.py
@@ -14,8 +14,8 @@ import os
 from datetime import timedelta
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+BASE_DIR = os.path.dirname(
+    os.path.dirname(os.path.abspath(os.path.join(__file__, '..'))))
 
 # Application definition
 
