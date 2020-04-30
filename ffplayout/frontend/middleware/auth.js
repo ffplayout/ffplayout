b43fe21b3d5e75722e524e12f720c26a432bf41c
Jonathan Baecker <jb@pixelcrusher.de>
2020-04-30 10:52:14 +0200

    fix direction

--- ffplayout/frontend/middleware/auth.js
+++ ffplayout/frontend/middleware/auth.js
@@ -1,5 +1,5 @@
 export default async function ({ store, redirect }) {
-    await store.dispatch('/auth/inspectToken')
+    await store.dispatch('auth/inspectToken')
 
     if (!store.state.auth.isLogin) {
         return redirect('/')
