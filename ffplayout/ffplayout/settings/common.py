e2367bb30badf2c4f41ab5df745a684190e67958
jb-alvarado <jb@pixelcrusher.de>
2020-06-04 21:17:27 +0200

    higher token live time, should fix #10

--- ffplayout/ffplayout/settings/common.py
+++ ffplayout/ffplayout/settings/common.py
@@ -90,7 +90,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # simple JWT auth settings
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=120),
+    'ACCESS_TOKEN_LIFETIME': timedelta(hours=7),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
 }
 
