e6c765486c12c343f455b58f98efc3a3a8504070
Jonathan Baecker <jb@pixelcrusher.de>
2020-08-26 09:27:24 +0200

    add babel compact

--- ffplayout/frontend/nuxt.config.js
+++ ffplayout/frontend/nuxt.config.js
@@ -100,6 +100,7 @@ export default {
         /*
          ** You can extend webpack config here
          */
-        extend(config, ctx) {}
+        extend(config, ctx) {},
+        babel: { compact: true }
     }
 }
