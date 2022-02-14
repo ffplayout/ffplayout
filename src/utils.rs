6970b58fd12f87f0dd9e7d97dc30148a29517d8b
jb-alvarado <jb@pixelcrusher.de>
2022-02-14 21:32:55 +0100

    cleanup

--- src/utils.rs
+++ src/utils.rs
@@ -3,11 +3,8 @@ use chrono::prelude::*;
 pub fn get_sec() -> f64 {
     let local: DateTime<Local> = Local::now();
 
-    let sec = (
-        local.hour() * 3600 + local.minute() * 60 + local.second()
-    ) as f64 + (local.nanosecond() as f64 / 1000000000.0);
-
-    sec
+    (local.hour() * 3600 + local.minute() * 60 + local.second()
+    ) as f64 + (local.nanosecond() as f64 / 1000000000.0)
 }
 
 pub fn get_timestamp() -> i64 {
