8583accc6cd466d1ee11370c71503aeca757f6c2
jb-alvarado <jb@pixelcrusher.de>
2022-02-16 21:24:33 +0100

    get next playlist

--- src/utils/playlist.rs
+++ src/utils/playlist.rs
@@ -1,23 +1,41 @@
-use crate::utils::get_config;
-use crate::utils::json_reader::{Program, read_json};
+use crate::utils::{
+    get_config,
+    json_reader::{read_json, Program},
+};
 
 pub struct CurrentProgram {
     nodes: Vec<Program>,
     idx: usize,
 }
 
+impl CurrentProgram {
+    fn new() -> Self {
+        let config = get_config();
+        let program: Vec<Program> = read_json(&config).program;
+        Self {
+            nodes: program,
+            idx: 0,
+        }
+    }
+}
+
 impl Iterator for CurrentProgram {
     type Item = Program;
 
     fn next(&mut self) -> Option<Self::Item> {
-        if self.idx == self.nodes.len() - 1 {
+        if self.idx < self.nodes.len() {
             let current = self.nodes[self.idx].clone();
-            self.idx = 0;
+            self.idx += 1;
 
             Some(current)
         } else {
-            let current = self.nodes[self.idx].clone();
-            self.idx += 1;
+            // play first item from next playlist
+            let config = get_config();
+            let program: Vec<Program> = read_json(&config).program;
+            self.nodes = program;
+            self.idx = 1;
+
+            let current = self.nodes[0].clone();
 
             Some(current)
         }
@@ -25,12 +43,5 @@ impl Iterator for CurrentProgram {
 }
 
 pub fn program() -> CurrentProgram {
-    let config = get_config();
-
-    let program: Vec<Program> = read_json(&config).program;
-
-    CurrentProgram {
-        nodes: program,
-        idx: 0,
-    }
+    CurrentProgram::new()
 }
