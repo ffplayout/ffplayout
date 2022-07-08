4429aeb1c6ee8d9579e34efd69fd4478216bfc92
jb-alvarado <jb@pixelcrusher.de>
2022-07-08 09:36:58 +0200

    better log info about write permission

--- ffplayout-engine/src/main.rs
+++ ffplayout-engine/src/main.rs
@@ -50,7 +50,7 @@ fn status_file(stat_file: &str, playout_stat: &PlayoutStatus) {
 
         let json: String = serde_json::to_string(&data).expect("Serialize status data failed");
         if let Err(e) = fs::write(stat_file, &json) {
-            error!("Unable to write status file: {e}");
+            error!("Unable to write to status file <b><magenta>{stat_file}</></b>: {e}");
         };
     } else {
         let stat_file = File::options()
