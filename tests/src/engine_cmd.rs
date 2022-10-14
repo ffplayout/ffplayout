a15c4576a20a11f98f3efa469c2610d41fed8861
jb-alvarado <jb@pixelcrusher.de>
2022-10-14 11:37:56 +0200

    add test logo

--- tests/src/engine_cmd.rs
+++ tests/src/engine_cmd.rs
@@ -10,7 +10,7 @@ use std::sync::{Arc, Mutex};
 fn video_audio_input() {
     let mut config = PlayoutConfig::new(Some("../assets/ffplayout.yml".to_string()));
     config.out.mode = Stream;
-    config.processing.logo = "../assets/logo.png".to_string();
+    config.processing.logo = "./assets/logo.png".to_string();
 
     let media_obj = Media::new(0, "./assets/with_audio.mp4", true);
     let media = gen_source(&config, media_obj, &Arc::new(Mutex::new(vec![])));
@@ -18,7 +18,7 @@ fn video_audio_input() {
     let test_filter_cmd = Some(
         vec_strings![
             "-filter_complex",
-            "[0:v:0]scale=1024:576,null[v];movie=../assets/logo.png:loop=0,setpts=N/(FRAME_RATE*TB),format=rgba,colorchannelmixer=aa=0.7[l];[v][l]overlay=W-w-12:12:shortest=1[vout0];[0:a:0]anull[aout0]",
+            "[0:v:0]scale=1024:576,null[v];movie=./assets/logo.png:loop=0,setpts=N/(FRAME_RATE*TB),format=rgba,colorchannelmixer=aa=0.7[l];[v][l]overlay=W-w-12:12:shortest=1[vout0];[0:a:0]anull[aout0]",
             "-map",
             "[vout0]",
             "-map",
