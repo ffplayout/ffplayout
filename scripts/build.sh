3a875b5c4d547cf446f9ba6e9477eef581161420
jb-alvarado <jb@pixelcrusher.de>
2024-01-03 23:04:49 +0100

    add macos

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,8 +10,7 @@ fi
 if [[ -n $target ]]; then
     targets=($target)
 else
-    targets=("x86_64-unknown-linux-musl" "aarch64-unknown-linux-gnu" "x86_64-pc-windows-gnu")
-    # "x86_64-apple-darwin" "aarch64-apple-darwin"
+    targets=("x86_64-unknown-linux-musl" "aarch64-unknown-linux-gnu" "x86_64-pc-windows-gnu" "x86_64-apple-darwin" "aarch64-apple-darwin")
 fi
 
 IFS="= "
