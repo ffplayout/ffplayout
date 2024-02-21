48d46f6098f77e1b4a2241f8277bc598fd2d700b
jb-alvarado <jb@pixelcrusher.de>
2024-02-21 17:34:10 +0100

    cleanup

--- stores/auth.ts
+++ stores/auth.ts
@@ -39,7 +39,6 @@ export const useAuth = defineStore('auth', {
 
             await $fetch<LoginObj>('/auth/login/', {
                 method: 'POST',
-                headers: new Headers([['content-type', 'application/json;charset=UTF-8']]),
                 body: JSON.stringify(payload),
                 async onResponse({ response }) {
                     code = response.status
