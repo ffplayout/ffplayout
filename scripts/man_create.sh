0f4bbd886313040eb1178925e7b68a92bbcb4091
jb-alvarado <jb@pixelcrusher.de>
2024-06-28 16:06:18 +0200

    fix paths

--- scripts/man_create.sh
+++ scripts/man_create.sh
@@ -5,16 +5,10 @@ yes | rm -f assets/ffplayout.1.gz
 engine_docs=(
     "README.md"
     "docs/api.md"
-    "ffplayout/README.md"
     "docs/install.md"
     "docs/output.md"
     "docs/live_ingest.md"
     "docs/preview_stream.md"
-)
-
-api_docs=(
-    "ffplayout-api/README.md"
-    "docs/api.md"
 )
 
 pandoc "${engine_docs[@]}" -s --wrap=preserve -t man -o assets/ffplayout.1
