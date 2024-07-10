c0d6f02d739f8554cf3a6c4ee090f8b346b26e31
jb-alvarado <jb@pixelcrusher.de>
2024-07-10 09:25:47 +0200

    update packages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,150 +1,154 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    devServer: {
-        port: 3000, // default: 3000
-        host: '127.0.0.1', // default: localhost
-    },
+  devServer: {
+      port: 3000, // default: 3000
+      host: '127.0.0.1', // default: localhost
+  },
 
-    nitro: {
-        devProxy: {
-            '/api': { target: 'http://127.0.0.1:8787/api' },
-            '/auth': { target: 'http://127.0.0.1:8787/auth' },
-            '/data': { target: 'http://127.0.0.1:8787/data' },
-            '/live': { target: 'http://127.0.0.1:8787/live' },
-            '/file': { target: 'http://127.0.0.1:8787/file' },
-            '/hls': { target: 'http://127.0.0.1:8787/hls' },
-        },
-    },
+  nitro: {
+      devProxy: {
+          '/api': { target: 'http://127.0.0.1:8787/api' },
+          '/auth': { target: 'http://127.0.0.1:8787/auth' },
+          '/data': { target: 'http://127.0.0.1:8787/data' },
+          '/live': { target: 'http://127.0.0.1:8787/live' },
+          '/file': { target: 'http://127.0.0.1:8787/file' },
+          '/hls': { target: 'http://127.0.0.1:8787/hls' },
+      },
+  },
 
-    ignore: ['**/public/tv-media**', '**/public/Videos**', '**/public/live**', '**/public/home**'],
-    ssr: false,
-    // debug: true,
+  ignore: ['**/public/tv-media**', '**/public/Videos**', '**/public/live**', '**/public/home**'],
+  ssr: false,
 
-    app: {
-        head: {
-            title: 'ffplayout',
-            meta: [
-                {
-                    charset: 'utf-8',
-                },
-                {
-                    name: 'viewport',
-                    content: 'width=device-width, initial-scale=1',
-                },
-                {
-                    hid: 'description',
-                    name: 'description',
-                    content: 'Frontend for ffplayout, the 24/7 Rust and playlist based streaming solution.',
-                },
-            ],
-            link: [
-                {
-                    rel: 'icon',
-                    type: 'image/x-icon',
-                    href: '/favicon.ico',
-                },
-            ],
-        },
-    },
+  // debug: true,
 
-    modules: [
-        '@nuxt/eslint',
-        '@nuxtjs/color-mode',
-        '@nuxtjs/tailwindcss',
-        '@nuxtjs/i18n',
-        '@pinia/nuxt',
-        '@vueuse/nuxt',
-    ],
-    css: ['@/assets/scss/main.scss'],
+  app: {
+      head: {
+          title: 'ffplayout',
+          meta: [
+              {
+                  charset: 'utf-8',
+              },
+              {
+                  name: 'viewport',
+                  content: 'width=device-width, initial-scale=1',
+              },
+              {
+                  hid: 'description',
+                  name: 'description',
+                  content: 'Frontend for ffplayout, the 24/7 Rust and playlist based streaming solution.',
+              },
+          ],
+          link: [
+              {
+                  rel: 'icon',
+                  type: 'image/x-icon',
+                  href: '/favicon.ico',
+              },
+          ],
+      },
+  },
 
-    colorMode: {
-        preference: 'dark', // default value of $colorMode.preference
-        fallback: 'system', // fallback value if not system preference found
-        hid: 'nuxt-color-mode-script',
-        globalName: '__NUXT_COLOR_MODE__',
-        componentName: 'ColorScheme',
-        classPrefix: '',
-        classSuffix: '',
-        dataValue: 'theme',
-        storageKey: 'theme',
-    },
+  modules: [
+      '@nuxt/eslint',
+      '@nuxtjs/color-mode',
+      '@nuxtjs/tailwindcss',
+      '@nuxtjs/i18n',
+      '@pinia/nuxt',
+      '@vueuse/nuxt',
+  ],
 
-    i18n: {
-        locales: [
-            {
-                code: 'de',
-                name: 'Deutsch',
-                file: 'de-DE.js',
-            },
-            {
-                code: 'en',
-                name: 'English',
-                file: 'en-US.js',
-            },
-            {
-                code: 'pt-br',
-                name: 'Português (BR)',
-                file: 'pt-BR.js',
-            },
-            {
-                code: 'ru',
-                name: 'Русский язык (RU)',
-                file: 'ru-RU.js',
-            },
-        ],
-        customRoutes: 'config',
-        pages: {
-            player: {
-                de: '/wiedergabe',
-                en: '/player',
-                'pt-br': '/player',
-                ru: '/player',
-            },
-            media: {
-                de: '/medien',
-                en: '/media',
-                'pt-br': '/armazenamento',
-                ru: '/media',
-            },
-            message: {
-                de: '/nachrichten',
-                en: '/message',
-                'pt-br': '/legenda',
-                ru: '/message',
-            },
-            logging: {
-                de: '/protokollierung',
-                en: '/logging',
-                'pt-br': '/registro',
-                ru: '/logging',
-            },
-            configure: {
-                de: '/einstellungen',
-                en: '/configure',
-                'pt-br': '/configurar',
-                ru: '/configure',
-            },
-        },
-        detectBrowserLanguage: {
-            useCookie: true,
-            alwaysRedirect: true,
-        },
-        // debug: true,
-        langDir: 'lang',
-        defaultLocale: 'en',
+  css: ['@/assets/scss/main.scss'],
 
-        compilation: {
-            strictMessage: false,
-        },
-    },
+  colorMode: {
+      preference: 'dark', // default value of $colorMode.preference
+      fallback: 'system', // fallback value if not system preference found
+      hid: 'nuxt-color-mode-script',
+      globalName: '__NUXT_COLOR_MODE__',
+      componentName: 'ColorScheme',
+      classPrefix: '',
+      classSuffix: '',
+      dataValue: 'theme',
+      storageKey: 'theme',
+  },
 
-    vite: {
-        build: {
-            chunkSizeWarningLimit: 800000,
-        },
-    },
+  i18n: {
+      locales: [
+          {
+              code: 'de',
+              name: 'Deutsch',
+              file: 'de-DE.js',
+          },
+          {
+              code: 'en',
+              name: 'English',
+              file: 'en-US.js',
+          },
+          {
+              code: 'pt-br',
+              name: 'Português (BR)',
+              file: 'pt-BR.js',
+          },
+          {
+              code: 'ru',
+              name: 'Русский язык (RU)',
+              file: 'ru-RU.js',
+          },
+      ],
+      customRoutes: 'config',
+      pages: {
+          player: {
+              de: '/wiedergabe',
+              en: '/player',
+              'pt-br': '/player',
+              ru: '/player',
+          },
+          media: {
+              de: '/medien',
+              en: '/media',
+              'pt-br': '/armazenamento',
+              ru: '/media',
+          },
+          message: {
+              de: '/nachrichten',
+              en: '/message',
+              'pt-br': '/legenda',
+              ru: '/message',
+          },
+          logging: {
+              de: '/protokollierung',
+              en: '/logging',
+              'pt-br': '/registro',
+              ru: '/logging',
+          },
+          configure: {
+              de: '/einstellungen',
+              en: '/configure',
+              'pt-br': '/configurar',
+              ru: '/configure',
+          },
+      },
+      detectBrowserLanguage: {
+          useCookie: true,
+          alwaysRedirect: true,
+      },
+      // debug: true,
+      langDir: 'lang',
+      defaultLocale: 'en',
 
-    experimental: {
-        payloadExtraction: false,
-    },
+      compilation: {
+          strictMessage: false,
+      },
+  },
+
+  vite: {
+      build: {
+          chunkSizeWarningLimit: 800000,
+      },
+  },
+
+  experimental: {
+      payloadExtraction: false,
+  },
+
+  compatibilityDate: '2024-07-10',
 })
