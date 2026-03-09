6c0a5662868d96b7f01230f03f5c4dccb2dd66a9
Jonathan Baecker <jonbae77@gmail.com>
2026-03-09 11:36:25 +0100

    update packages

--- engine/build.rs
+++ engine/build.rs
@@ -1,11 +1,15 @@
+use std::env;
+
 use static_files::NpmBuild;
 
 fn main() -> std::io::Result<()> {
     if !cfg!(debug_assertions) && cfg!(feature = "embed_frontend") {
+        let target_path = env::current_dir()?.join("../frontend/dist");
+
         NpmBuild::new("../")
             .install()?
             .run("build")?
-            .target("../frontend/dist")
+            .target(target_path)
             .change_detection()
             .to_resource_dir()
             .build()
