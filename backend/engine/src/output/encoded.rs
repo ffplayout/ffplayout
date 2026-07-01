c37ded486c88577a3850459cccd35d699d4334ea
jb-alvarado <jb@pixelcrusher.de>
2026-07-01 21:25:31 +0200

    add preset to EncodedFormat::Auto

--- backend/engine/src/output/encoded.rs
+++ backend/engine/src/output/encoded.rs
@@ -464,11 +464,16 @@ fn open_video_stream(
         video_ctx.set_flags(video_flags);
     }
 
+    let mut options = ffmpeg::Dictionary::new();
+
     // TODO: make preset configurable
     let video_encoder = match output_format {
-        EncodedFormat::Auto | EncodedFormat::Null => video_ctx.open_as(video_codec)?,
+        EncodedFormat::Auto => {
+            options.set("preset", "faster");
+            video_ctx.open_as_with(video_codec, options)?
+        }
+        EncodedFormat::Null => video_ctx.open_as(video_codec)?,
         EncodedFormat::Hls { .. } => {
-            let mut options = ffmpeg::Dictionary::new();
             options.set("x264-params", "open-gop=0:repeat-headers=1");
             options.set("preset", "faster");
             video_ctx.open_as_with(video_codec, options)?
